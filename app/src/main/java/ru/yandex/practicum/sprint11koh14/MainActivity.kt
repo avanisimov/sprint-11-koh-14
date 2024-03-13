c2db680498aeaef4457269e6b5d052ce379849e0
Aleksei Anisimov <suxelshock@gmail.com>
2024-03-13 10:22:41 +0300

    Загрузка новостей

--- app/src/main/java/ru/yandex/practicum/sprint11koh14/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint11koh14/MainActivity.kt
@@ -43,37 +43,62 @@ class MainActivity : AppCompatActivity() {
         val itemsRv: RecyclerView = findViewById(R.id.items)
         itemsRv.adapter = adapter
 
+        val classs : Class<Date> = Date::class.java
+
+        val str = "asdasd"
+        val strClass : Class<String> = str.javaClass
+        strClass.fields.forEach { field ->
+
+        }
+        classs.methods.forEach {
+            it.name
+            it.annotations
+        }
+        classs.fields.forEach {
+            val fieldType: Class<*> = it.type
+            if (fieldType == classs) {
+                CustomDateTypeAdapter()
+            }
+        }
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://raw.githubusercontent.com/avanisimov/sprint-11-koh-14/")
             .addConverterFactory(
                 GsonConverterFactory.create(
                     GsonBuilder()
                         .registerTypeAdapter(Date::class.java, CustomDateTypeAdapter())
+                        .registerTypeAdapter(NewsItem::class.java, NewsItemAdapter())
                         .create()
                 )
             )
             .build()
         val serverApi = retrofit.create(Sprint11ServerApi::class.java)
 
-        serverApi.getNews1().enqueue(object : Callback<NewsResponse> {
-            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
-                Log.i(TAG, "onResponse: ${response.body()}")
-                adapter.items = response.body()?.data?.items ?: emptyList()
-            }
+        serverApi.getNews1()
+            .enqueue(object : Callback<NewsResponse> {
+                override fun onResponse(
+                    call: Call<NewsResponse>,
+                    response: Response<NewsResponse>
+                ) {
+                    Log.i(TAG, "onResponse: ${response.body()}")
+                    adapter.items = response.body()?.data?.items?.filter {
+                        it !is NewsItem.Unknown
+                    } ?: emptyList()
+                }
 
-            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
-                Log.e(TAG, "onFailure: $t")
-            }
+                override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                    Log.e(TAG, "onFailure: $t")
+                }
 
-        })
+            })
     }
 }
 
-// https://raw.githubusercontent.com/avanisimov/sprint-11-koh-9/main/jsons/news_1.json
+// https://raw.githubusercontent.com/avanisimov/sprint-11-koh-14/master/jsons/news_1.json
 
 interface Sprint11ServerApi {
 
 
-    @GET("main/jsons/news_1.json")
+    @GET("master/jsons/news_2.json")
     fun getNews1(): Call<NewsResponse>
 }
--- app/src/main/java/ru/yandex/practicum/sprint11koh14/models.kt
+++ app/src/main/java/ru/yandex/practicum/sprint11koh14/models.kt
@@ -1,8 +1,14 @@
 package ru.yandex.practicum.sprint11koh14
 
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
 import com.google.gson.TypeAdapter
+import com.google.gson.annotations.SerializedName
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonWriter
+import java.lang.IllegalStateException
+import java.lang.reflect.Type
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -17,13 +23,36 @@ data class Data(
     val items: List<NewsItem>
 )
 
-data class NewsItem(
-    val id: String,
-    val title: String,
-    val type: String,
-    val created: Date,
-)
+sealed class NewsItem {
+    abstract val id: String
+    abstract val title: String
+    abstract val type: String
+    abstract val created: Date
 
+    data class Sport(
+        override val id: String,
+        override val title: String,
+        override val type: String,
+        override val created: Date,
+        val specificPropertyForSport: String,
+    ) : NewsItem()
+
+    data class Science(
+        override val id: String,
+        override val title: String,
+        override val type: String,
+        override val created: Date,
+        @SerializedName("specific_property_for_science")
+        val specificPropertyForScience: String,
+    ) : NewsItem()
+
+    data class Unknown(
+        override val id: String,
+        override val title: String,
+        override val type: String,
+        override val created: Date,
+    ) : NewsItem()
+}
 
 class CustomDateTypeAdapter : TypeAdapter<Date>() {
 
@@ -39,6 +68,23 @@ class CustomDateTypeAdapter : TypeAdapter<Date>() {
 
     override fun read(`in`: JsonReader): Date {
         return formatter.parse(`in`.nextString())
+    }
+
+}
+
+class NewsItemAdapter : JsonDeserializer<NewsItem> {
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
+    ): NewsItem {
+        val typeJsonElement = json.asJsonObject.get("type")
+        val type: String = typeJsonElement.asJsonPrimitive.asString
+        return when (type) {
+            "sport" -> context.deserialize(json, NewsItem.Sport::class.java)
+            "science" -> context.deserialize(json, NewsItem.Science::class.java)
+            else -> context.deserialize(json, NewsItem.Unknown::class.java)
+        }
     }
 
 }
